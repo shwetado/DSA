7ec771438511a405b86cb13358c5cd675e91a726
Shweta <shwetadongare28@gmail.com>
2013-12-16 00:23:09 +0530

    without memcopy

--- stack/Stack.c
+++ stack/Stack.c
@@ -1,49 +1,46 @@
 #include "stack.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <memory.h>
 
-Stack* create(int sizeOfElement,int maxElements){
-    Stack *stack = calloc(sizeof(Stack),1);
-    stack->base = calloc(maxElements,sizeOfElement);
-    stack->sizeOfElement = sizeOfElement;
-    stack->maxElements = maxElements;
-    stack->top = -1;
+Stack* create(int length){
+    Stack* stack = calloc(sizeof(Stack), 1);
+    stack->elements = calloc(sizeof(void*), length);
+    stack->top = 0;
+    stack->length = length;
     return stack;
 };
 
-int push(Stack *stack,void *element){
-    void* newElement;
-    if(isFull(stack))        
-    	return 0;
-    newElement = stack->base+((stack->top+1)*stack->sizeOfElement);
-    memcpy (newElement, element, stack->sizeOfElement);
-    stack->top++;
+int isFull(Stack* stack){
+    return stack->top == stack->length;
+};
+
+void** getElement(Stack* stack,int top){
+    return stack->elements + top * sizeof(stack->elements);
+};
+
+int push(Stack* stack,void* element){
+    void* temp;
+    if(isFull(stack)){
+        temp = realloc(stack->elements, stack->length * 2 * sizeof(void*));
+        if(!temp)
+            return 0;
+        stack->elements = temp;
+        stack->length *= 2;
+    }
+    *(getElement(stack, stack->top++)) = element;
     return 1;
 };
 
-void* pop(Stack *stack){
-    void* element;
-    if(isEmpty(stack))        
-    	return NULL;
-    element = stack->base+(stack->top*stack->sizeOfElement);
-    stack->top--;
-    return element;
+int isEmpty(Stack* stack){
+    return stack->top == 0;
 };
 
-void* top(Stack *stack){
-    void* element = stack->base+(stack->top*stack->sizeOfElement);
-    return element;
+void* pop(Stack* stack){
+    if(isEmpty(stack)) return NULL;
+    return *(getElement(stack,--(stack->top)));
 };
 
-int isEmpty(Stack *stack){
-    if(stack->top==-1)        
-    	return true;
-    return false; 
+void* top(Stack* stack){
+    if(isEmpty(stack)) return NULL;
+    return *(getElement(stack, (stack->top-1)));
 };
-
-int isFull(Stack *stack){
-    if(stack->top==stack->maxElements-1)        
-    	return true;
-    return false; 
-}
--- stack/Stack.h
+++ stack/Stack.h
@@ -1,19 +1,17 @@
-#include <stdbool.h>
-typedef struct{
-    void *base;
+typedef struct {
+    void** elements;
     int top;
-    int sizeOfElement;
-    int maxElements;
+    int length;
 } Stack;
 
 typedef char String[256];
 
-Stack* create(int sizeOfElement,int maxElements);
+Stack* create(int length);
+void** getElement(Stack* stack,int top);
+int push(Stack* stack,void* element);
+void* pop(Stack* stack);
 
-int isEmpty(Stack *stack);
-int isFull(Stack *stack);
+void* top(Stack* stack);
 
-int push(Stack *stack,void *element);
-void* pop(Stack *stack);
-
-void* top(Stack *stack);
+int isEmpty(Stack* stack);
+int isFull(Stack* stack);
